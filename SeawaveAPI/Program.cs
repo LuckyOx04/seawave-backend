61a6726e96d878de517379e2e8957a20e59af5b0
LuckyOx04 <ivailon27@gmail.com>
2026-05-03 18:39:56 +0300

    Initial setup.

--- SeawaveAPI/Program.cs
+++ SeawaveAPI/Program.cs
@@ -1,6 +1,22 @@
+using DataAccess;
+using DataAccess.Repositories;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException($"Connection string not found.");
+
+builder.Services.AddSingleton<IDbConnectionFactory>(_ => new MySqlDbConnectionFactory(connectionString));
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<SessionRepository>();
+builder.Services.AddScoped<MusicRepository>();
+
+builder.Services.AddScoped<EmailService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<PasswordService>();
+builder.Services.AddScoped<MusicService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
