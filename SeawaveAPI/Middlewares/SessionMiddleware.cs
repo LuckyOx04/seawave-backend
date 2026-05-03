e862c9f66aa78f69bc3d9df43f3138bb6c674af2
LuckyOx04 <ivailon27@gmail.com>
2026-05-03 20:33:02 +0300

    Added session middleware, which checks the session token validity.

---
+++ SeawaveAPI/Middlewares/SessionMiddleware.cs
@@ -0,0 +1,23 @@
+using Services;
+
+namespace SeawaveAPI.Middlewares;
+
+public class SessionMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context, AuthService authService)
+    {
+        string? token = context.Request.Headers["Authorization"];
+
+        if (!string.IsNullOrEmpty(token))
+        {
+            int? userId = await authService.ValidateSessionAsync(token);
+
+            if (userId.HasValue)
+            {
+                context.Items["UserId"] = userId.Value;
+            }
+
+            await next(context);
+        }
+    }
+}
