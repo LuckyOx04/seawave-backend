70f890e141297cb9a7981bdf219f42adb621a625
LuckyOx04 <ivailon27@gmail.com>
2026-05-05 19:02:14 +0300

    Cleaned up some controller methods.

--- SeawaveAPI/Controllers/AuthController.cs
+++ SeawaveAPI/Controllers/AuthController.cs
@@ -12,11 +12,14 @@ public class AuthController(AuthService authService) : ControllerBase
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegistrationRequest req) 
         => Ok(await authService.RegisterAsync(req));
-    
+
     [HttpGet("confirm-email")]
-    public async Task<IActionResult> ConfirmEmail([FromQuery] string token) 
-        => Ok(await authService.ConfirmEmailAsync(token));
-    
+    public async Task<IActionResult> ConfirmEmail([FromQuery] string token)
+    {
+        var success = await authService.ConfirmEmailAsync(token);
+        return success ? Ok() : BadRequest();
+    }
+
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequest req) => Ok(await authService.LoginAsync(req));
 
--- SeawaveAPI/Controllers/PasswordController.cs
+++ SeawaveAPI/Controllers/PasswordController.cs
@@ -15,18 +15,22 @@ public class PasswordController(PasswordService passwordService) : ControllerBas
         await passwordService.RequestPasswordResetAsync(request.Email);
         return Ok("A reset link was sent to the email.");
     }
-    
+
     [HttpPost("reset")]
-    public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordRequest request) 
-        => Ok(await passwordService.ResetPasswordAsync(request.Token, request.NewPassword));
+    public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordRequest request)
+    {
+        var success = await passwordService.ResetPasswordAsync(request.Token, request.NewPassword);
+        return success ? Ok() : Forbid();
+    }
 
     [HttpPatch("change")]
     [SessionAuthorize]
     public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequest request)
     {
-        int userId = (int)HttpContext.Items["UserId"]!;
+        var userId = (int)HttpContext.Items["UserId"]!;
         
-        return Ok(await passwordService.ChangePasswordAsync(userId, request.CurrentPassword,
-            request.NewPassword, request.ConfirmPassword));
+        var success = await passwordService.ChangePasswordAsync(userId, request.CurrentPassword,
+            request.NewPassword, request.ConfirmPassword);
+        return success ? Ok() : Forbid();
     }
 }
