72a27692edf0cd050c6b74b97855271b53530f69
LuckyOx04 <ivailon27@gmail.com>
2026-05-05 17:54:45 +0300

    Changed MusicController to use request DTOs.

--- SeawaveAPI/Controllers/MusicController.cs
+++ SeawaveAPI/Controllers/MusicController.cs
@@ -1,3 +1,4 @@
+using Core.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using SeawaveAPI.Attributes;
 using Services;
@@ -9,13 +10,14 @@ namespace SeawaveAPI.Controllers;
 public class MusicController(MusicService musicService) : ControllerBase
 {
     [HttpGet("search/tracks")]
-    public async Task<IActionResult> SearchTracks(string q) => Ok(await musicService.SearchTrackAsync(q));
+    public async Task<IActionResult> SearchTracks([FromQuery] string q) => Ok(await musicService.SearchTrackAsync(q));
     
     [HttpGet("search/playlists")]
-    public async Task<IActionResult> SearchPlaylists(string q) => Ok(await musicService.SearchPlaylistAsync(q));
+    public async Task<IActionResult> SearchPlaylists([FromQuery] string q) 
+        => Ok(await musicService.SearchPlaylistAsync(q));
 
     [HttpGet("stream/{fileName}")]
-    public async Task<IActionResult> StreamTrack(string fileName)
+    public async Task<IActionResult> StreamTrack([FromRoute] string fileName)
     {
         var stream = await musicService.GetTrackStreamAsync(fileName);
         return File(stream, "audio/mpeg", enableRangeProcessing: true);
@@ -23,10 +25,12 @@ public class MusicController(MusicService musicService) : ControllerBase
 
     [HttpPost("upload")]
     [SessionAuthorize]
-    public async Task<IActionResult> UploadTrack(int userId, string title, string artist, IFormFile file)
+    public async Task<IActionResult> UploadTrack([FromBody] TrackUploadRequest request)
     {
-        using var stream = file.OpenReadStream();
-        await musicService.RequestUploadAsync(userId, title, artist, file.FileName, stream);
-        return Ok();
+        var userId = (int)HttpContext.Items["UserId"]!;
+        
+        await using var stream = request.File.OpenReadStream();
+        await musicService.RequestUploadAsync(userId, request.Title, request.Artist, request.File.FileName, stream);
+        return Ok("File uploaded for review.");
     }
 }
